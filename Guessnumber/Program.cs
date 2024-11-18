e586911e1189c73a7a23101e10681c282b0dbbab
marcushoja0108 <marcushj@live.no>
2024-11-18 13:34:05 +0100

    Add project files.

---
+++ Guessnumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+
+bool gameOn = true;
+while (gameOn == true)
+{
+    Random rand = new Random();
+    int rng = rand.Next(1, 101);
+    int attempts = 0;
+
+    Console.WriteLine("Guess a number between 1 and 100");
+
+
+    bool asked = true;
+
+    while (asked == true)
+    {
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        attempts++;
+
+        if (userInput < rng)
+        {
+            Console.WriteLine("Too low");
+        }
+        else if (userInput > rng)
+        {
+            Console.WriteLine("Too high");
+        }
+        else
+        {
+            Console.WriteLine($"THATS RIGHT, {rng} WAS THE NUMBER! ");
+            Console.WriteLine($"Number of guesses: {attempts}");
+            Console.WriteLine("Do you want to play again? y/n");
+            asked = false;
+        }
+    }
+
+    string replay = Console.ReadLine();
+
+    if (replay == "n")
+    {
+        Console.WriteLine("Ok, bye then ;.;");
+        gameOn = false;
+    }
+}
